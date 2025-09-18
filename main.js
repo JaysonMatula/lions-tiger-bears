18ca5b212fa24932c6497e8b51f5b948131102b1
JaysonMatula <114369233+JaysonMatula@users.noreply.github.com>
2025-09-18 17:09:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -17,10 +17,14 @@ function createBreedList(breedList) {
   `
 }
 
-function loadByBreed(breed) {
+async function loadByBreed(breed) {
   if (breed != "Choose a dog breed") {
   const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
   const data = await response.json()
-  console.log(data)
+  createSlideshow(data.message)
   }
+}
+
+function createSlideshow(images) {
+  document
 }
