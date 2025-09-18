a19503e91947e05b7f205f91d7119640e3b7d5cb
JaysonMatula <114369233+JaysonMatula@users.noreply.github.com>
2025-09-18 11:08:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello from our JS")
+async function start() {
+  const response = await fetch(https://dog.ceo/api/breeds/list/all)
+  const data = await response.json()
+  createBreedList(data.message)
+}
+
+start()
+
+function createBreedList(breedList) {
+}
