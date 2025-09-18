e14039a09b5279fb9f26aadeaee7d6ef67848bbd
JaysonMatula <114369233+JaysonMatula@users.noreply.github.com>
2025-09-18 18:56:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function createBreedList(breedList) {
 }
 
 async function loadByBreed(breed) {
-  if (breed != "Choose a dog breed") {
+  if (breed !== "Choose a dog breed") {
   const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
   const data = await response.json()
   createSlideshow(data.message)
