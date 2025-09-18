ece09fc4483d53088da24a215502ff0d55ad0559
JaysonMatula <114369233+JaysonMatula@users.noreply.github.com>
2025-09-18 17:57:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -26,8 +26,23 @@ async function loadByBreed(breed) {
 }
 
 function createSlideshow(images) {
+  let currentPosition = 0
   document.getElementById("slideshow").innerHTML = `
     <div class="slide" style="background-image: url('${images[0]}')"></div>
     <div class="slide" style="background-image: url('${images[1]}')"></div>
   `
+  currentPosition += 2
+  setInterval(nextSlide, 3000)
+
+  function nextSlide() {
+    document.getElementById("slideshow").insertAdjacentHTML("beforeend", ` <div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
+    setTimeout(function () {
+      document.querySelector(".slide").remove()
+    }, 1000)
+    if (currentPosition + 1 >= images.length) {
+      currentPosition = 0
+    } else {
+      currentPosition++
+    }
+  }
 }
